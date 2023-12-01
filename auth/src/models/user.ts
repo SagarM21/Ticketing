3f1aa27970988f42df62fbd8ce9662b5be4618cd
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-12-02 00:02:39 +0530

    add: BadRequestError  and mongoose presave hooks

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,12 +1,18 @@
 import mongoose, { mongo } from "mongoose";
+import { Password } from "../services/password";
 
 interface UserAttrs {
   email: string;
   password: string;
 }
 
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any;
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
 }
 
 const userSchema = new mongoose.Schema({
@@ -20,10 +26,19 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.pre("save", async function (done) {
+  // even if you are creating the password for the very first time, isModified will return true
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
+});
+
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-const User = mongoose.model<any, UserModel>("User", userSchema);
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
 
 export { User };
