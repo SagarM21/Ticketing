82b5a21c5f489f7a43d90c44c275495a120af62a
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-12-01 23:17:53 +0530

    add: connected to mongoDB

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/signin";
 import { signOutRouter } from "./routes/signout";
@@ -22,6 +24,16 @@ app.all("*", async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on PORT 3000!");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to mongo db");
+  } catch (error) {
+    console.error(error);
+  }
+  app.listen(3000, () => {
+    console.log("Listening on PORT 3000!");
+  });
+};
+
+start();
