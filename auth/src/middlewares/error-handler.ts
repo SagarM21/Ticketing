4c03848a9d73899ebd87ef766f21dc37a5a57ca4
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-12-01 18:19:03 +0530

    add: subclassing for custom errors

---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log("Something went wrong", err);
+
+  res.send({ message: err.message });
+};
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 const router = express.Router();
 
 router.post(
@@ -12,14 +14,16 @@ router.post(
       .withMessage("Password must be between 4 and 20 characters"),
   ],
   (req: Request, res: Response) => {
-    const errors = validationResult(req)
+    const errors = validationResult(req);
 
-    if(!errors.isEmpty()){
-      return res.status(400).send(errors.array())
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
     }
-    const { email, password } = req.body;
-    
-    
+    // const { email, password } = req.body;
+
+    console.log("Creating a user");
+    throw new DatabaseConnectionError();
+    res.send({});
   }
 );
 
