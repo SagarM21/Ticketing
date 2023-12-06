0d70617290fc4fb3c358429a750c9d9cd692dda8
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-12-06 22:07:36 +0530

    add: authProtection in tickets

---
+++ tickets/src/routes/new.ts
@@ -0,0 +1,9 @@
+import { requireAuth } from "@sagarm21tickets/common";
+import express, { Request, Response } from "express";
+const router = express.Router();
+
+router.post("/api/tickets", requireAuth, (req: Request, res: Response) => {
+  res.sendStatus(200);
+});
+
+export { router as createTicketRouter };
