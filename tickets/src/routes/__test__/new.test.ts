b50c88cd34fa3d7ad0aea48079a9f86cadfec67d
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-12-06 22:41:08 +0530

    add: ticket create ready

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
 
 it("has a route handler listening to /api/tickets for post requests", async () => {
   const response = await request(app).post("/api/tickets").send({});
@@ -57,13 +58,19 @@ it("returns an error if an invalid price is provided", async () => {
 });
 
 it("creates a ticket with valid inputs", async () => {
-    
-    await request(app)
-      .post("/api/tickets")
-      .set("Cookie", global.signin())
-      .send({
-        title: "Sagar",
-        price: 20,
-      })
-      .expect(201);
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "Sagar",
+      price: 20,
+    })
+    .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  
 });
