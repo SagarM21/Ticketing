f120ea3f7e0cc71d1574d029cd955ea3cee5bb5f
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-12-06 21:52:15 +0530

    shift: mongo uri to env

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,8 +3,9 @@ import { app } from "./app";
 
 const start = async () => {
   if (!process.env.JWT_KEY) throw new Error("JWT key must be defined");
+  if (!process.env.MONGO_URI) throw new Error("MONGO URI must be defined");
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to mongo db");
   } catch (error) {
     console.error(error);
