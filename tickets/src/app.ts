264c2ad0d37fc84f88a382ddf26947e96e976273
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-12-06 23:07:30 +0530

    add: getTickets route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -9,6 +9,7 @@ import {
   NotFoundError,
 } from "@sagarm21tickets/common";
 import { createTicketRouter } from "./routes/new";
+import { showTickerRouter } from "./routes/show";
 
 const app = express();
 app.set("trust proxy", true);
@@ -21,6 +22,7 @@ app.use(
 );
 
 app.use(currentUser);
+app.use(showTickerRouter);
 app.use(createTicketRouter);
 
 app.all("*", async (req, res) => {
