25e8c7a6f0c71eed4581306b865f37d9724a7456
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-12-07 17:01:28 +0530

    add: accessing event data

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats from "node-nats-streaming";
+import nats, { Message } from "node-nats-streaming";
 
 console.clear();
 
@@ -10,7 +10,11 @@ client.on("connect", () => {
   console.log("Listener connected to NATS");
 
   const subscription = client.subscribe("ticket:created");
-  subscription.on("message", (msg) => {
-    console.log("Message Received");
+  subscription.on("message", (msg: Message) => {
+    // console.log("Message Received", msg);
+    const data = msg.getData();
+    if (typeof data === "string") {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
   });
 });
