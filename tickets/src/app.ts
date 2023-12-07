104291b1e37490bf6c6029b5a306adfc621a5e12
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-12-07 15:29:13 +0530

    complete: index router

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -10,6 +10,7 @@ import {
 } from "@sagarm21tickets/common";
 import { createTicketRouter } from "./routes/new";
 import { showTickerRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
 
 const app = express();
 app.set("trust proxy", true);
@@ -24,6 +25,7 @@ app.use(
 app.use(currentUser);
 app.use(showTickerRouter);
 app.use(createTicketRouter);
+app.use(indexTicketRouter)
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
