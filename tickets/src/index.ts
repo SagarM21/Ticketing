ad11cb95abd98f33b36723c3faf9b16a287b8702
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-12-09 17:12:24 +0530

    add: graceful exit of nats

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -7,6 +7,15 @@ const start = async () => {
   if (!process.env.MONGO_URI) throw new Error("MONGO URI must be defined");
   try {
     await natsWrapper.connect("ticketing", "clientId", "http://nats-srv:4222");
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to mongo db");
   } catch (error) {
