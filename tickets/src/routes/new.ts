90b10df801cf3a4b736138d9190bf05b75f0f50d
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-12-09 17:05:01 +0530

    add: accessing nats client

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import { body } from "express-validator";
 import express, { Request, Response } from "express";
 import { Ticket } from "../models/ticket";
 import { TicketCreatedPublisher } from "../events/publisher/ticket-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 const router = express.Router();
 
 router.post(
@@ -20,7 +21,7 @@ router.post(
 
     const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
     await ticket.save();
-    new TicketCreatedPublisher(client).publish({
+    new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
       title: ticket.title,
       price: ticket.price,
