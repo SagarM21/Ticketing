e6ec4c1c5d166d50b5602ef31a3a38dc17a1c352
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-12-10 15:35:36 +0530

    add: whether the ticket is reserved?

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,15 @@
 import express, { Request, Response } from "express";
-import { requireAuth, validateRequest } from "@sagarm21tickets/common";
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from "@sagarm21tickets/common";
 import { body } from "express-validator";
 import mongoose from "mongoose";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
@@ -17,7 +25,13 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    res.send({});
+    const { ticketId } = req.body;
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) throw new NotFoundError();
+
+    const isReserved = await ticket.isReserved();
+    if (isReserved) throw new BadRequestError("Ticket is already reserved");
+    
   }
 );
 
