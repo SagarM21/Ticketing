3f24274667c5a3f14073f7994e237861206a5ec8
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-12-17 18:43:37 +0530

    fix: client side bug fix

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -6,7 +6,7 @@ const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
       <Header currentUser={currentUser} />
-      <Component {...pageProps} />
+      <Component {...pageProps} currentUser={currentUser} />
     </div>
   );
 };
@@ -18,7 +18,11 @@ AppComponent.getInitialProps = async (appContext) => {
   // invoking the getInitialProp of landing page
   let pageProps = {};
   if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    pageProps = await appContext.Component.getInitialProps(
+      appContext.ctx,
+      client,
+      data.currentUser
+    );
   }
 
   console.log(data);
